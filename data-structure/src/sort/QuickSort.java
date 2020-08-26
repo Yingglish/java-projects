37403faefa0687551559d70507fdb06c29b860d9
Yingglish <1085963924@qq.com>
2020-08-26 22:37:04 +0800

    快速排序

--- data-structure/src/sort/QuickSort.java
+++ data-structure/src/sort/QuickSort.java
@@ -1,7 +1,14 @@
 package sort;
 
+import sort.ref.DataWrap;
+
+/**
+ * 基本思想: 从待排序的数据队列中任取一个数据作为分界值，所有比它小的数据元素放左边，比它大的放右边
+ * 		接着对左右俩个子序列进行递归
+ * 	组 A[p…r] 被划分为两个子数组 A[p…q-1] 和 A[q+1…r]，使得 A[p…q-1] 中每个元素都小于等于 A[q]，而 A[q+1…r] 每个元素都大于 A[q]
+ */
 public class QuickSort {
-	private static void swap(DataWrap[] data, int i,int j) {
+	private static void swap(DataWrap[] data, int i, int j) {
 		DataWrap tmp = data[i];
 		data[i] = data[j];
 		data[j] = tmp;
@@ -12,7 +19,7 @@ public class QuickSort {
 	private static void subSort(DataWrap[] data, int start, int end) {
 		// 需要排序
 		if (start < end) {
-			// 以以一个元素作为分界值
+			// 取一个元素作为分界值
 			DataWrap base = data[start];
 			// i 从左边开始搜索，搜索大于分界值的元素的索引
 			int i = start;
@@ -21,6 +28,7 @@ public class QuickSort {
 			while (true) {
 				// 找到大于分界值元素的索引，或i已经到达end处
 				while (i < end && data[++i].compareTo(base) <= 0);
+				// 找到小于分界值元素的索引，或j已经到达start处
 				while (j > start && data[--j].compareTo(base) >=0);
 				if(i < j) {
 					swap(data, i ,j);
