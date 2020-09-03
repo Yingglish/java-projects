4670869f662e2f2d5d245bc3abbed4641830748e
Yingglish <1085963924@qq.com>
2020-09-03 22:41:23 +0800

    尚硅谷 银行项目

---
+++ oop/src/com/yingglish/bankproject/OverdraftException.java
@@ -0,0 +1,21 @@
+package com.yingglish.bankproject;
+
+public class OverdraftException extends RuntimeException {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    //赤字
+    private double deficit;
+
+    public double getDeficit() {
+        return deficit;
+    }
+
+    public OverdraftException(String message, double deficit) {
+        super(message);
+        this.deficit = deficit;
+    }
+}
---
+++ oop/src/com/yingglish/bankproject/SavingAccount.java
@@ -0,0 +1,11 @@
+package com.yingglish.bankproject;
+
+public class SavingAccount extends Account{
+    //利率
+    private double interestRate;
+
+    public SavingAccount(double init_balance, double interestRate) {
+        super(init_balance);
+        this.interestRate = interestRate;
+    }
+}
